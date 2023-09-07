59de539e8360beeeb700bb0478cf03e9009f04a9
John David Molina <jd_v@yahoo.com>
2023-09-07 19:16:46 -0300

    paquetes creados

---
+++ UniversidadGrupo70/src/universidadgrupo70/UniversidadGrupo70.java
@@ -0,0 +1,25 @@
+/*
+	Trabajo práctico trasversal de la Guía 5 del curso Desarrollo de Apps
+	Universidad de La Punta en el marco del proyecto Argentina Programa 4.0
+
+	Integrantes:
+		John David Molina Velarde
+		Leticia Mores
+		Enrique Germán Martínez
+		Carlos Eduardo Beltrán
+ */
+
+package universidadgrupo70;
+
+/**
+ *
+ * @author John David Molina Velarde, Leticia Mores, Enrique Germán Martínez, Carlos Eduardo Beltrán
+ */
+public class UniversidadGrupo70 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+	
+}
