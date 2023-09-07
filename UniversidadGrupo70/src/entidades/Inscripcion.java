e6204caf0e00865b2ace10fe1baf041a1388bd2d
John David Molina <jd_v@yahoo.com>
2023-09-07 19:26:34 -0300

    Archivos vacíos de entidades Alumno, Materia e Inscripcion creados

---
+++ UniversidadGrupo70/src/entidades/Inscripcion.java
@@ -0,0 +1,19 @@
+/*
+	Trabajo práctico trasversal de la Guía 5 del curso Desarrollo de Apps
+	Universidad de La Punta en el marco del proyecto Argentina Programa 4.0
+
+	Integrantes:
+		John David Molina Velarde
+		Leticia Mores
+		Enrique Germán Martínez
+		Carlos Eduardo Beltrán
+ */
+package entidades;
+
+/**
+ *
+ * @author John David Molina Velarde, Leticia Mores, Enrique Germán Martínez, Carlos Eduardo Beltrán
+ */
+public class Inscripcion {
+	
+}
