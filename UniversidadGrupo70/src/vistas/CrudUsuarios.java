9b28f555594383fb5cbc14afaccaf9a5c345fe05
John David Molina <jd_v@yahoo.com>
2023-09-28 15:52:56 -0300

    Usuarios activo

--- UniversidadGrupo70/src/vistas/CrudUsuarios.java
+++ UniversidadGrupo70/src/vistas/CrudUsuarios.java
@@ -497,14 +497,14 @@ public class CrudUsuarios extends javax.swing.JInternalFrame {
 
             },
             new String [] {
-                "Id", "DNI", "Apellido", "Nombre", "Nacimiento", "Activo"
+                "Id", "Clave", "Apellido", "Nombre"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.Integer.class, java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.Object.class, java.lang.Boolean.class
+                java.lang.Integer.class, java.lang.Integer.class, java.lang.String.class, java.lang.String.class
             };
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, false, false
+                false, false, false, false
             };
 
             public Class getColumnClass(int columnIndex) {
@@ -527,8 +527,6 @@ public class CrudUsuarios extends javax.swing.JInternalFrame {
             tablaUsuarios.getColumnModel().getColumn(1).setPreferredWidth(30);
             tablaUsuarios.getColumnModel().getColumn(2).setPreferredWidth(80);
             tablaUsuarios.getColumnModel().getColumn(3).setPreferredWidth(80);
-            tablaUsuarios.getColumnModel().getColumn(4).setPreferredWidth(20);
-            tablaUsuarios.getColumnModel().getColumn(5).setPreferredWidth(10);
         }
 
         lblTituloTabla.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
